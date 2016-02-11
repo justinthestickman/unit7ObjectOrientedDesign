e643a470fcfab41b86dcddd11aea635bfe83750a
justinthestickman <jchuang@NN123-HA-09.HS.sd203.org>
2016-02-11 15:08:20 -0600

    Started Triangle Program

--- Chapter 10 Practice/Person.java
+++ Chapter 10 Practice/Person.java
@@ -23,20 +23,18 @@ public class Person implements Comparable<Person>
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Person> people = new ArrayList<Person>();        
+        Person[] people = new Person[10];        
         for (int i = 0; i < 10; i++)
         {
             System.out.print("Name of Person #" + (i + 1) + ": ");
-            String name = scanner.next();
-            Person person = new Person(name);
-            people.add(person);
+            people[i] = new Person(scanner.next());
         }
         
-        Person first = people.get(0);
-        Person last = people.get(0);
-        for (int j = 1; j < people.size(); j++)
+        Person first = people[0];
+        Person last = people[0];
+        for (int j = 1; j < 10; j++)
         {
-            Person person = people.get(j);
+            Person person = people[j];
             
             if (person.compareTo(first) < 0)
             {
---
+++ Chapter 10 Practice/TriangleDrawer.java
@@ -0,0 +1,36 @@
+// import java.awt.event.MouseListener;
+// import java.awt.event.MouseEvent;
+// import javax.swing.JFrame;
+// import java.awt.Graphics;
+// import java.awt.Graphics2D;
+// import java.awt.Rectangle;
+// import javax.swing.JComponent;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TriangleDrawer
+{
+    private static final int FRAME_WIDTH = 1000;
+    private static final int FRAME_HEIGHT = 750;
+    
+    public TriangleDrawer()
+    {
+        
+    }
+    
+    public class MousePressListener implements MouseListener
+    {
+        public void mousePressed(MouseEvent event)
+        {
+            int x = event.getX();
+            int y = event.getY();
+        }
+        
+        public void mouseReleased(MouseEvent event){}
+        public void mouseClicked(MouseEvent event){}
+        public void mouseEntered(MouseEvent event){}
+        public void mouseExited(MouseEvent event){}
+    }
+}
